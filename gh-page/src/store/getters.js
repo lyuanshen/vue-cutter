ec9043c5ddb5eef4e513f07ea998652a66efe6d1
LYuanshen <707978562@qq.com>
2020-09-01 01:27:10 +0800

    rebu

--- gh-page/src/store/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-  device: state => state.app.device,
-  lang: state => state.app.lang,
-  dashboardJs: state => state.code.dashboardJs,
-  dashboardVue:state => state.code.dashboardVue
-};
-export default getters
