a161ca37f54a03d76d3ce3c2aafd2ab8696428a4
LYuanshen <707978562@qq.com>
2020-09-02 01:02:40 +0800

    init version2Dev

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <template>
-<!--  开发测试-->
+<!--  dev test -->
     <div id="app">
-        <vue-cutter
-          :container-width="600"
-          image="https://cdn1-images.epio.app/image/download/aHR0cHM6Ly93d3cubXlteXBpYy5uZXQvZGF0YS9hdHRhY2htZW50L2ZvcnVtLzIwMTgwMS8wOC8wMzQ5MjdkMXRzcXQ2cjZtZHkyeHZkLmpwZy50aHVtYi5qcGc=/350X500"
-        ></vue-cutter>
+
     </div>
 </template>
 
 <script>
     export default {
-        name: 'app',
-        data() {
-            return {
-                img: ''
-            }
-        },
-        methods: {
-            getCropperAxis() {
-                let axis = this.$refs.cropper.getCropBlob((data) => {
-                    let url = (window.URL || window.webkitURL).createObjectURL(data);
-                    this.img = url;
-                })
-            },
-            addImage() {
-                this.$refs.cropper.addImage()
-            }
-        }
+        name: 'app'
+
     }
 </script>
 
---
+++ src/lib/styles/vue-cutter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vue-cutter"
+    }
+</script>
+
+<style scoped>
+
+</style>
